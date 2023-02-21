7471b5abee50ecb6ccbf2bf462d58816249659b0
Paula Zaidel <paulazaaidel@gmail.com>
2023-02-21 14:12:51 -0300

    SEO

--- app/public/wp-content/themes/bikcraft/header.php
+++ app/public/wp-content/themes/bikcraft/header.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <meta charset="utf-8">
-        <title>Bikcraft - Bicicletas Personalizadas</title>
-        <meta name="description" content="Compre a sua bicicleta personalizada na Bikcraft. Possuímos modelos Passeio, Retrô e Esporte.">
-        <meta property="og:type" content="website"/>
-        <meta property="og:title" content="Bikcraft - Bicicletas Personalizadas"/>
-        <meta property="og:description" content="Compre a sua bicicleta personalizada na Bikcraft. Possuímos modelos Passeio, Retrô e Esporte."/>
-        <meta property="og:url" content="http://bikcraft.com"/>
-        <meta property="og:image" content="http://bikcraft.com/img/og-image.png"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="shortcut icon" href="favicon.ico">
+    <meta charset="utf-8">
+		<title><?php bloginfo('name'); ?> <?php wp_title('-'); ?> <?php the_field('title_seo'); ?></title>
+		<meta name="description" content="<?php bloginfo('name'); ?> <?php wp_title('-'); ?> <?php the_field('description_seo'); ?>">
+
+		<meta property="og:type" content="website"/>
+		<meta property="og:title" content="<?php bloginfo('name'); ?> <?php wp_title('-'); ?> <?php the_field('title_seo'); ?>"/>
+		<meta property="og:description" content="<?php bloginfo('name'); ?> <?php wp_title('-'); ?> <?php the_field('description_seo'); ?>"/>
+		<meta property="og:url" content="<?php bloginfo('url'); ?>"/>
+		<meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/img/og-image.png"/>
+
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
 
 		<!-- Start Wordpress Header -->
 		<?php wp_head(); ?>
